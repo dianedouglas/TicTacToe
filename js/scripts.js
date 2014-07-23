f8d724881646d28f3d7ac91725cc6a0f57c86290
Amoreena Hagedorn & Diane Douglas <student@epicodus.com>
2014-07-23 10:43:57 -0700

    Create a markedBy property to be set by a mark method inside of the Space object. markedBy will be either X, O or 0 for a blank space.

--- js/scripts.js
+++ js/scripts.js
@@ -8,9 +8,14 @@ var Player = {
 var Space = {
   xCoordinate: 0,
   yCoordinate: 0,
+  markedBy: 0,
   initialize: function(coordinate1, coordinate2) {
     this.xCoordinate = coordinate1;
     this.yCoordinate = coordinate2;
+    this.markedBy = 0;
+  },
+  mark: function(XO) {
+    this.markedBy = XO;
   }
 }
 
@@ -19,6 +24,10 @@ $(document).ready(function(){
   var player2 = Object.create(Player);
   player1.initialize("X");
   player2.initialize("O");
+
+  // var space1 =Object.create(Space);
+  // space1.initialize(0,0);
+  // space1.mark(player1.symbol);
 
 
 })
--- specs/specs.js
+++ specs/specs.js
@@ -15,8 +15,21 @@ describe("Space", function() {
       testSpace.initialize(1, 2);
       testSpace.xCoordinate.should.equal(1);
       testSpace.yCoordinate.should.equal(2);
+      testSpace.markedBy.should.equal(0);
     });
   });
+
+  describe("mark", function() {
+    it("sets the markedBy property to either X or O", function() {
+      var testSpace = Object.create(Space);
+      testSpace.xCoordinate = 1;
+      testSpace.yCoordinate = 2;
+      testSpace.markedBy = 0;
+      testSpace.mark("X");
+      testSpace.markedBy.should.equal("X");
+    });
+  });
+
 });
 
   // describe("create"); // you write the rest of this one!
